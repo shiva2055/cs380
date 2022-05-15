4e4c7d3c3827d7d3d0825bde7596004afa647d63
DragonReborn96 <katreynolds94@gmail.com>
2022-05-15 16:13:20 -0700

    commit on main

--- client/src/components/pages/Account/EditableUserProfile.js
+++ client/src/components/pages/Account/EditableUserProfile.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import '../../css/Home.css';
 import '../../css/Account.css';
-import Stack from '@mui/material/Stack'
-import { TextField, Grid, Button, FormControl, withStyles } from "@material-ui/core";
+import { TextField, Grid, Button, FormControl, withStyles  } from "@material-ui/core";
+import {Stack, InputLabel, MenuItem, Select} from '@mui/material'
 import { Link, useNavigate } from "react-router-dom";
+
 
 export default function EditableUserProfile({
     stored,
@@ -180,15 +181,69 @@ export default function EditableUserProfile({
         </Grid>
 
         <Grid item xs={1} sm={1} md={1} lg={1} xl={1}> 
-          <TextField
-            id="filled-required"
-            label="State*" 
-            variant="filled" 
-            size="small"
-            type={Text}
-            value={state}
-            onChange={e => setState(e.target.value)}
-          />
+          <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="demo-simple-select-filled-label">State</InputLabel>
+            <Select
+              labelId="demo-simple-select-filled-label"
+              id="demo-simple-select-filled"
+              size="small"
+              value={state}
+              label="State"
+              onChange={e => setState(e.target.value)}
+            >
+              <MenuItem value=""><em>Select</em></MenuItem>
+              <MenuItem value={"AL"}>Alabama</MenuItem>
+              <MenuItem value={"AK"}>Alaska</MenuItem>
+              <MenuItem value={"AZ"}>Arizona</MenuItem>
+              <MenuItem value={"AR"}>Arkansas</MenuItem>
+              <MenuItem value={"CA"}>California</MenuItem>
+              <MenuItem value={"CO"}>Colorado</MenuItem>
+              <MenuItem value={"CT"}>Connecticut</MenuItem>
+              <MenuItem value={"DE"}>Delaware</MenuItem>
+              <MenuItem value={"FL"}>Florida</MenuItem>
+              <MenuItem value={"GA"}>Georgia</MenuItem>
+              <MenuItem value={"HI"}>Hawaii</MenuItem>
+              <MenuItem value={"ID"}>Idaho</MenuItem>
+              <MenuItem value={"IL"}>Illinois</MenuItem>
+              <MenuItem value={"IN"}>Indiana</MenuItem>
+              <MenuItem value={"IA"}>Iowa</MenuItem>
+              <MenuItem value={"KS"}>Kansas</MenuItem>
+              <MenuItem value={"KY"}>Kentucky</MenuItem>
+              <MenuItem value={"LA"}>Louisiana</MenuItem>
+              <MenuItem value={"ME"}>Maine</MenuItem>
+              <MenuItem value={"MD"}>Maryland</MenuItem>
+              <MenuItem value={"MA"}>Massachusetts</MenuItem>
+              <MenuItem value={"MI"}>Michigan</MenuItem>
+              <MenuItem value={"MN"}>Minnesota</MenuItem>
+              <MenuItem value={"MS"}>Mississippi</MenuItem>
+              <MenuItem value={"MO"}>Missouri</MenuItem>
+              <MenuItem value={"MT"}>Montana</MenuItem>
+              <MenuItem value={"NE"}>Nebraska</MenuItem>
+              <MenuItem value={"NV"}>Nevada</MenuItem>
+              <MenuItem value={"NH"}>New Hampshire</MenuItem>
+              <MenuItem value={"NJ"}>New Jersey</MenuItem>
+              <MenuItem value={"NM"}>New Mexico</MenuItem>
+              <MenuItem value={"NY"}>New York</MenuItem>
+              <MenuItem value={"NC"}>North Carolina</MenuItem>
+              <MenuItem value={"ND"}>North Dakota</MenuItem>
+              <MenuItem value={"OH"}>Ohio</MenuItem>
+              <MenuItem value={"OK"}>Oklahoma</MenuItem>
+              <MenuItem value={"OR"}>Oregon</MenuItem>
+              <MenuItem value={"PA"}>Pennsylvania</MenuItem>
+              <MenuItem value={"RI"}>Rhode Island</MenuItem>
+              <MenuItem value={"SC"}>South Carolina</MenuItem>
+              <MenuItem value={"SD"}>South Dakota</MenuItem>
+              <MenuItem value={"TN"}>Tennessee</MenuItem>
+              <MenuItem value={"TX"}>Texas</MenuItem>
+              <MenuItem value={"UT"}>Utah</MenuItem>
+              <MenuItem value={"VT"}>Vermont</MenuItem>
+              <MenuItem value={"VA"}>Virginia</MenuItem>
+              <MenuItem value={"WA"}>Washington</MenuItem>
+              <MenuItem value={"WV"}>West Virginia</MenuItem>
+              <MenuItem value={"WI"}>Wisconsin</MenuItem>
+              <MenuItem value={"WY"}>Wyoming</MenuItem>
+            </Select>
+          </FormControl>
         </Grid>
       </Grid>
       
