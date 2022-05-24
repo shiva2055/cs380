21ab8daee5e35dea3bac667d6759b60283484d7c
DragonReborn96 <katreynolds94@gmail.com>
2022-05-23 23:14:50 -0700

    commit on main

--- client/src/components/pages/Account/Reservations.js
+++ client/src/components/pages/Account/Reservations.js
@@ -2,62 +2,50 @@ import React from "react";
 import '../../css/Home.css';
 import '../../css/Account.css';
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from "@mui/material";
-import { rootShouldForwardProp } from "@mui/material/styles/styled";
 import { Link, useNavigate } from "react-router-dom";
 
-function createData(
-    orderNum,
-    datePlaced, 
-    reservationDate, 
-    partyNum, 
-    restaruant
-    ) {
+function createData(orderNum, datePlaced, reservationDate, restaraunt, party) {
+    return { orderNum, datePlaced, reservationDate, restaraunt, party };
+  }
+  
+  const rows = [
+    createData('E1234', '05/23/2022', '05/30/2022', 'Olive Garden', 5),
+    createData('D1234', '05/01/2022', '05/15/2022', 'Outback Steakhouse', 2),
+    createData('C1234', '04/20/2022', '04/28/2022', 'The Melting Pot', 2),
+    createData('B1234', '03/15/2022', '03/25/2022', "Bob's Burger and Brew", 7),
+    createData('A1234', '01/07/2022', '01/10/2022', 'Applebees', 2),
+  ];
+  
+  export default function BasicTable() {
     return (
-        orderNum,
-        datePlaced, 
-        reservationDate, 
-        restaruant,
-        partyNum   
-        )
-}
-
-const rows = [
-    createData(1234, 5/22/2022, 5/30/2022, "Olive Garden", 5),
-];
-
-export default function Reservations() {
-    
-    console.log("Reservations");
-
-    return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Order #</TableCell>
-                        <TableCell align="right">Date Placed</TableCell>
-                        <TableCell align="right">Reservation Date</TableCell>
-                        <TableCell alight="right">Restaruant</TableCell>
-                        <TableCell align="right">Party Size</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.orderNum}
-                            </TableCell>
-                            <TableCell align="right">{row.datePlaced}</TableCell>
-                            <TableCell align="right">{row.reservationDate}</TableCell>
-                            <TableCell align="right">{row.restaruant}</TableCell>
-                            <TableCell align="right">{row.partyNum}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell><h3>Order #</h3></TableCell>
+              <TableCell align="right"><h3>Date Placed</h3></TableCell>
+              <TableCell align="right"><h3>Reservation Date</h3></TableCell>
+              <TableCell align="right"><h3>Restaraunt</h3></TableCell>
+              <TableCell align="right"><h3>Party Size</h3></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.orderNum}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.orderNum}
+                </TableCell>
+                <TableCell align="right">{row.datePlaced}</TableCell>
+                <TableCell align="right">{row.reservationDate}</TableCell>
+                <TableCell align="right">{row.restaraunt}</TableCell>
+                <TableCell align="right">{row.party}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     );
-}
+  }
